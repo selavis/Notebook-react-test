ee9b4b24b909085821200008d21f6be0a7a679a1
selavis <50261993+selavis@users.noreply.github.com>
2022-03-17 23:11:53 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import uuid from "react-uuid";
 import "./App.css";
 import Main from "./main/Main";
@@ -6,65 +6,57 @@ import Sidebar from "./sidebar/Sidebar";
 
 function App() {
   const [notes, setNotes] = useState(
-    localStorage.notes ? JSON.parse(localStorage.notes) : []
+    window.localStorage.getItem("notes")
+      ? JSON.parse(window.localStorage.getItem("notes"))
+      : []
   );
-  const [activeNote, setActiveNote] = useState(false);
-  const [filter, setFilter] = useState(notes || []);
+
+  const [activeNote, setActiveNote] = useState(null);
 
   useEffect(() => {
     localStorage.setItem("notes", JSON.stringify(notes));
   }, [notes]);
 
-  const onAddNote = () => {
+  const handleActiveChange = (activeNote) =>
+    setActiveNote(notes.find((note) => note.id === activeNote.id));
+
+  const handleNoteAdd = () => {
     const newNote = {
       id: uuid(),
       title: "Note title",
-      category: "Category",
+      category: "",
       body: "",
       lastModified: Date.now(),
     };
-
     setNotes([newNote, ...notes]);
-    setActiveNote(newNote.id);
+    setActiveNote(newNote);
   };
 
-  const onDeleteNote = (noteId) => {
-    setNotes(notes.filter(({ id }) => id !== noteId));
-  };
-
-  const onUpdateNote = (updatedNote) => {
-    const updatedNotesArr = notes.map((note) => {
-      if (note.id === updatedNote.id) {
-        return updatedNote;
-      }
-      return note;
-    });
-
-    setNotes(updatedNotesArr);
-  };
-
-  const getActiveNote = () => {
-    return notes.find(({ id }) => id === activeNote);
-  };
-
-  const handleFilterList = (value) => {
-    const search = notes.filter(
-      (note) => note.title.toLowerCase().search(value) !== -1
+  const handleNoteEdit = (editedNote) => {
+    setNotes(
+      notes.map((note) => (note.id === editedNote.id ? editedNote : note))
     );
-    setFilter(search.length ? search : notes);
+  };
+
+  const handleNoteDelete = (toDeleteId) => {
+    if (activeNote?.id === toDeleteId) {
+      setTimeout(() => {
+        setActiveNote(null);
+      });
+    }
+    setNotes(notes.filter(({ id }) => id !== toDeleteId));
   };
 
   return (
     <div className="App">
       <Sidebar
-        notes={filter}
-        onAddNote={onAddNote}
-        onDeleteNote={onDeleteNote}
-        activeNote={activeNote}
-        setActiveNote={setActiveNote}
-        onNoteSearch={handleFilterList}
+        notes={notes}
+        active={activeNote?.id}
+        addNote={handleNoteAdd}
+        setActive={handleActiveChange}
+        deleteNote={handleNoteDelete}
       />
-      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
+      <Main active={activeNote} editNote={handleNoteEdit} />
     </div>
   );
 }
