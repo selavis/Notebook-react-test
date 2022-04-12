7f88a2de68d0ff610b626022afceece7a19433f8
selavis <selavisproduction@gmail.com>
2022-04-12 10:21:55 +0200

    eventButton

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -13,7 +13,7 @@ const Main = ({ active, editNote }) => {
 
   const handleUpdate = (key, value) => setOnEdit({ ...onEdit, [key]: value });
 
-  const Button = () => {
+  const eventButton = () => {
     setOnEdit(null);
   };
 
