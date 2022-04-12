344b45069a5260ec1bc041f57578ed649fb5de8a
selavis <selavisproduction@gmail.com>
2022-04-12 10:57:30 +0200

    test 2 button

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -13,7 +13,7 @@ const Main = ({ active, editNote }) => {
 
   const handleUpdate = (key, value) => setOnEdit({ ...onEdit, [key]: value });
 
-  const renamedButton = () => {
+  const Test2Button = () => {
     setOnEdit(null);
   };
 
